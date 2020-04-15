b349c801d825cb1e35119d42acf902c679ede17f
BiancaNL <bianca.vandepoel@gmail.com>
2020-04-15 08:22:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
+	<link href="css/style.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<title>Onepager Title</title>
+	<meta content="Here goes the meta description to describe the sites contents" name="description">
 </head>
 <body>
 </body>
