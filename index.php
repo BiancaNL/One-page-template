1c8274b6dc9b9564693a10838c1c4ef211a2eef1
BiancaNL <bianca.vandepoel@gmail.com>
2020-05-03 12:02:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-	<link href="css/style.css" rel="stylesheet">
-	<meta charset="utf-8">
-	<meta content="width=device-width, initial-scale=1" name="viewport">
-	<title>Onepager Title</title>
-	<meta content="Here goes the meta description to describe the sites contents" name="description">
+
+<link href="assets/css/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap" rel="stylesheet">
+
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+
+<title>Building beautiful</title>
+<meta content="A clean and minimal html template with as little code as possible" name="description">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
 </head>
+
 <body>
 </body>
 </html>
